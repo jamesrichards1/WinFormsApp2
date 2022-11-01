32378b90d0b73afaf40b64cb9b50db7058461ae8
JR <jarichards123@gmail.com>
2022-11-01 16:38:08 +0000

    Add project files.

---
+++ WinFormsApp2/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WinFormsApp2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.sayHelloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(21, 19);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(113, 30);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(21, 80);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(112, 30);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(142, 19);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(179, 35);
+            this.firstNameText.TabIndex = 1;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(142, 80);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(179, 35);
+            this.lastNameText.TabIndex = 2;
+            // 
+            // sayHelloButton
+            // 
+            this.sayHelloButton.Location = new System.Drawing.Point(74, 152);
+            this.sayHelloButton.Name = "sayHelloButton";
+            this.sayHelloButton.Size = new System.Drawing.Size(205, 95);
+            this.sayHelloButton.TabIndex = 3;
+            this.sayHelloButton.Text = "Say Hello";
+            this.sayHelloButton.UseVisualStyleBackColor = true;
+            this.sayHelloButton.Click += new System.EventHandler(this.sayHelloButton_Click_1);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 317);
+            this.Controls.Add(this.sayHelloButton);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label firstNameLabel;
+        private Label lastNameLabel;
+        private TextBox firstNameText;
+        private TextBox lastNameText;
+        private Button sayHelloButton;
+    }
+}
---
+++ WinFormsApp2/MainForm.cs
@@ -0,0 +1,40 @@
+namespace WinFormsApp2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sayHelloButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lastNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sayHelloButton_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameText.Text} {lastNameText.Text}");
+        }
+    }
+}
